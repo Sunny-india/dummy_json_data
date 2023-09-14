64f32a16c7fd1e1301a25f94b792991e6554b7e3
itc <shrichawlaplastics14@gmail.com>
2023-09-15 00:11:33 +0530

    user model created

--- lib/models/quotes_model.dart
+++ lib/models/quotes_model.dart
@@ -61,14 +61,14 @@ class SecondModel {
 class Comments {
   int id, postId;
   String body;
-  User user;
+  UserInComments user;
   Comments(
       {required this.id,
       required this.postId,
       required this.body,
       required this.user});
   factory Comments.fromJson(Map<String, dynamic> json) {
-    User inside = User.fromJson(json['user']);
+    UserInComments inside = UserInComments.fromJson(json['user']);
     return Comments(
         id: json['id'],
         postId: json['postId'],
@@ -77,13 +77,13 @@ class Comments {
   }
 }
 
-class User {
+class UserInComments {
   int id;
   String username;
-  User({required this.id, required this.username});
+  UserInComments({required this.id, required this.username});
 
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(id: json['id'], username: json['username']);
+  factory UserInComments.fromJson(Map<String, dynamic> json) {
+    return UserInComments(id: json['id'], username: json['username']);
   }
 }
 
@@ -142,7 +142,7 @@ class Posts {
 /// Forth set starts here
 
 class ForthJson {
-  List<Carts> carts;
+  List<Cart> carts;
   int total, skip, limit;
   ForthJson(
       {required this.carts,
@@ -151,10 +151,10 @@ class ForthJson {
       required this.limit});
 
   factory ForthJson.fromJson(Map<String, dynamic> json) {
-    List<Carts> endList = [];
+    List<Cart> endList = [];
     List<Map<String, dynamic>> inner = json['carts'];
     inner.map((eachMap) {
-      endList.add(Carts.fromJson(eachMap));
+      endList.add(Cart.fromJson(eachMap));
     }).toList();
     return ForthJson(
         total: json['total'],
@@ -164,12 +164,12 @@ class ForthJson {
   }
 }
 
-class Carts {
+class Cart {
   int id, userId;
   double discountedTotal, total, totalProducts, totalQuantity;
   List<Products> products;
 
-  Carts(
+  Cart(
       {required this.id,
       required this.userId,
       required this.discountedTotal,
@@ -178,7 +178,7 @@ class Carts {
       required this.totalQuantity,
       required this.products});
 
-  factory Carts.fromJson(Map<String, dynamic> json) {
+  factory Cart.fromJson(Map<String, dynamic> json) {
     List<Products> endList = [];
     List<Map<String, dynamic>> inner =
         json['products'] as List<Map<String, dynamic>>;
@@ -191,7 +191,7 @@ class Carts {
       endList.add(Products.fromJson(eachMap));
     }).toList();
 
-    return Carts(
+    return Cart(
         id: json['id'],
         userId: json['userId'],
         discountedTotal: json['discountedTotal'],
@@ -228,3 +228,176 @@ class Products {
 }
 
 /// Forth set ends here .
+
+/// Fifth set starts here.
+
+class User {
+  int id, age;
+  String firstName,
+      lastName,
+      maidenName,
+      gender,
+      email,
+      phone,
+      username,
+      password,
+      birthDate,
+      image,
+      bloodGroup,
+      eyeColor,
+      domain,
+      ip,
+      macAddress,
+      university,
+      ein,
+      ssn,
+      userAgent;
+  double height, weight;
+  Hair hair;
+  Address address;
+  Bank bank;
+  Company company;
+  User(
+      {required this.id,
+      required this.age,
+      required this.birthDate,
+      required this.bloodGroup,
+      required this.height,
+      required this.domain,
+      required this.ein,
+      required this.email,
+      required this.eyeColor,
+      required this.firstName,
+      required this.lastName,
+      required this.maidenName,
+      required this.gender,
+      required this.image,
+      required this.ip,
+      required this.macAddress,
+      required this.password,
+      required this.phone,
+      required this.ssn,
+      required this.university,
+      required this.userAgent,
+      required this.username,
+      required this.weight,
+      required this.address,
+      required this.bank,
+      required this.company,
+      required this.hair});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    Address insideAddress = Address.fromJson(json['address']);
+    Company insideCompany = Company.fromJson(json['company']);
+    Bank insideBank = Bank.fromJson(json['bank']);
+    Hair insideHair = Hair.fromJson(json['hair']);
+    return User(
+        id: json['id'],
+        age: json['age'],
+        birthDate: json['birthDate'],
+        bloodGroup: json['bloodGroup'],
+        domain: json['domain'],
+        ein: json['ein'],
+        email: json['email'],
+        eyeColor: json['eyeColor'],
+        firstName: json['firstName'],
+        maidenName: json['maidenName'],
+        lastName: json['lastName'],
+        height: json['height'],
+        weight: json['weight'],
+        gender: json['gender'],
+        image: json['image'],
+        ip: json['ip'],
+        macAddress: json['macAddress'],
+        password: json['password'],
+        phone: json['phone'],
+        ssn: json['ssn'],
+        username: json['username'],
+        userAgent: json['userAgent'],
+        university: json['university'],
+        address: insideAddress,
+        company: insideCompany,
+        bank: insideBank,
+        hair: insideHair);
+  }
+}
+
+class Hair {
+  String color, type;
+  Hair({required this.color, required this.type});
+  factory Hair.fromJson(Map<String, dynamic> json) {
+    return Hair(color: json['color'], type: json['type']);
+  }
+}
+
+class Address {
+  String address, city, postalCode, state;
+  Coordinate coordinate;
+  Address(
+      {required this.address,
+      required this.city,
+      required this.postalCode,
+      required this.state,
+      required this.coordinate});
+
+  factory Address.fromJson(Map<String, dynamic> json) {
+    Coordinate inner = Coordinate.fromJson(json['coordinate']);
+
+    return Address(
+        address: json['address'],
+        city: json['city'],
+        postalCode: json['postalCode'],
+        state: json['state'],
+        coordinate: inner);
+  }
+}
+
+class Coordinate {
+  double lat, long;
+
+  Coordinate({required this.lat, required this.long});
+
+  factory Coordinate.fromJson(Map<String, dynamic> json) {
+    return Coordinate(lat: json['lat'], long: json['long']);
+  }
+}
+
+class Bank {
+  String cardExpire, cardNumber, cardType, currency, iban;
+  Bank(
+      {required this.cardExpire,
+      required this.cardNumber,
+      required this.cardType,
+      required this.currency,
+      required this.iban});
+
+  factory Bank.fromJson(Map<String, dynamic> json) {
+    return Bank(
+        cardExpire: json['cardExpire'],
+        cardNumber: json['cardNumber'],
+        cardType: json['cardType'],
+        currency: json['currency'],
+        iban: json['iban']);
+  }
+}
+
+class Company {
+  String department, name, title;
+  Address address;
+  Company(
+      {required this.department,
+      required this.name,
+      required this.title,
+      required this.address});
+
+  factory Company.fromJson(Map<String, dynamic> json) {
+    Address innerAddress = Address.fromJson(json['address']);
+    return Company(
+        department: json['department'],
+        name: json['name'],
+        title: json['title'],
+        address: innerAddress);
+  }
+}
+
+/// fifth set ends here
